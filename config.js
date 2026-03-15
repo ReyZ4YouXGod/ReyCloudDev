15632b9b5f5c3785d4783e508c107b14774df72a
ReyZ4YouXGod <raihanananda767@gmail.com>
2026-03-16 00:30:34 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+const config = {
+    token: "7148854426:AAGm74zljPqAPEMJELGiTGaHi5U_InIJpT4", 
+    chTestiId: "-1002597815366",
+    panel: "https://g.top4top.io/p_3718gga3x4.png", 
+    pakasir: {
+        apiKey: "9AwCqt0h99ArK0Jy7R5PYpP1FmdQ0SWN",
+        project: "reyclouddev"
+    },
+    resellerPrice: 7000,
+    domain: "https://reytzy.panel-ptrodactyl.my.id",
+    plta: "ptla_1wmZDgM46tVA3Rq8fyCua3SEh82ovv6AqzDbnbdooqr",
+    eggs: "15", nest: "5", loc: "1",
+  
+    prices: {
+        '1gb': 1000, '2gb': 2000, '3gb': 3000, '4gb': 4000, 
+        '5gb': 5000, '6gb': 6000, '7gb': 7000, '8gb': 8000, 
+        '9gb': 9000, '10gb': 10000, 'unli': 13000
+    }, 
+  
+    resourceMap: {
+        '1gb':  { ram: 1024,  disk: 10240, cpu: 100 },
+        '2gb':  { ram: 2048,  disk: 20480, cpu: 200 },
+        '3gb':  { ram: 3072,  disk: 30720, cpu: 300 },
+        '4gb':  { ram: 4096,  disk: 40960, cpu: 400 },
+        '5gb':  { ram: 5120,  disk: 51200, cpu: 500 },
+        '6gb':  { ram: 6144,  disk: 61440, cpu: 600 },
+        '7gb':  { ram: 7168,  disk: 71680, cpu: 700 },
+        '8gb':  { ram: 8192,  disk: 81920, cpu: 800 },
+        '9gb':  { ram: 9216,  disk: 92160, cpu: 900 },
+        '10gb': { ram: 10240, disk: 102400, cpu: 1000 },
+        'unli': { ram: 0,     disk: 0,      cpu: 0 }
+    }
+};
+module.exports = config;
